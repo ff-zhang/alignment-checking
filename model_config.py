378308aa93ba00ce9471a92ffbc4e20db53d816d
Rudy <rudraksh.monga@mail.utoronto.ca>
2024-03-20 14:50:56 -0400

    did stuff

--- model_config.py
+++ model_config.py
@@ -49,9 +49,12 @@ class ModelConfig:
 
             self.layers.append(temp)
 
+        self.model = None
+
 
     def build(self):
-        return Model(self)
+        self.model = Model(self)
+        return self.model
 
 
 class Model(nn.Module):
@@ -59,13 +62,38 @@ class Model(nn.Module):
     layer_type_map = {
         "Linear": nn.Linear
     }
-    
+
     act_map = {
         "ReLU": nn.ReLU,
         "Tanh": nn.Tanh,
         "Sigmoid": nn.Sigmoid
     }
 
-    def __init__(self, ModelConfig):
+    def __init__(self, modelConfig):
         super().__init__()
 
+        layers = []
+        for i in range(modelConfig.num_layers):
+            temp = modelConfig.num_layers[i]
+            layer = nn.Sequential(
+                Model.layer_type_map[temp["layer_type"]](in_features=temp["in_dim"], out_features=temp["out_dim"]),
+                Model.act_map[temp["act"]](),
+                nn.Dropout(p=temp["dropout"])
+            )
+
+            is_bn = temp.batch_norm
+            num_feat = temp["in_dim"]
+
+            layers.append((layer, is_bn, num_feat))
+
+
+    def forward(self, x):
+        # assume X is already in the correct shape and all that
+        out = x
+        
+        for layer in self.layers:
+            if layer[1]:
+                out = nn.BatchNorm1d(layer[2])
+            out = layer[0](out)
+
+        return x
